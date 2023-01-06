12d08f95c6ddf019de01b4b556cf00dda1461f84
Sergio Serrano <sergioandresgmd10@gmail.com>
2023-01-05 21:32:36 -0500

    Config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [  "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
+  content: [  "./app/**/*.{js,ts,jsx,tsx}", 
   "./pages/**/*.{js,ts,jsx,tsx}",
   "./components/**/*.{js,ts,jsx,tsx}",],
   theme: {
